bc4c60728ff0e7b484dc665c8a42cb85a8413a75
Saurabh Giri <81623624+saurabh9136@users.noreply.github.com>
2024-03-22 12:04:34 +0530

    Add files via upload

---
+++ List_data_structure/listSlicing.py
@@ -0,0 +1,8 @@
+"""
+List Slicing
+Similar to String Slicing
+list_name starting_idx: ending_idx] #ending idx is not included
+"""
+marks = [87, 64, 33, 95, 76]
+print(marks[1:4])
+print(marks[-3:-1])
