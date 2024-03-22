666448346418374a6befaea99800a2e1e12cb2ba
Saurabh Giri <81623624+saurabh9136@users.noreply.github.com>
2024-03-22 12:04:12 +0530

    commit

---
+++ List_data_structure/list.py
@@ -0,0 +1,12 @@
+"""
+Lists in Python
+A built-in data type that stores set of values
+It can store elements of different types (integer, float, string, etc.)
+"""
+# to declare a list use []
+
+
+marks = [87, 64, 33, 95, 76] #marks[0], marks[1]..
+student = ["Karan", 85, "Delhi"] #student[0], student[1]..
+student[0] = "Arjun" #allowed in python
+len(student) #returns length 
