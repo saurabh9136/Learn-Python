da6bea94e1f6af8cf1a69040110976dc9f6daa4f
Saurabh Giri <81623624+saurabh9136@users.noreply.github.com>
2024-03-22 12:04:55 +0530

    Add files via upload

---
+++ List_data_structure/methods.py
@@ -0,0 +1,24 @@
+list = [3, 5, 1, 8, 6]
+
+#add element in end
+list.append(24)
+
+#to sort in ascending order
+list.sort()
+
+#to sort in descending order
+list.sort(reverse=True)
+
+# to reverse list
+list.reverse()
+
+#add element at index
+list.insert(2, 4)
+
+#remove first occurance of element
+list.remove(3)
+
+#remove at element index
+list.pop(2)
+
+print(list)
