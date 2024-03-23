561db60d500ad0e77e62999a98e31ed4398f5e33
Saurabh Giri <81623624+saurabh9136@users.noreply.github.com>
2024-03-23 11:31:34 +0530

    Add files via upload

---
+++ Tuple_data_structure/methods.py
@@ -0,0 +1,9 @@
+# Tuple methods
+tup = (2, 1, 4, 1)
+
+#return index of first occurance tup.index(el)
+
+print(tup.index(4))
+
+#tup.count(el) #counts total occurrences tup.count(1) is 2
+print(tup.count(1))
