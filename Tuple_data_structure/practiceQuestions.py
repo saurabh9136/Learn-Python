dd7a360a975f66a594328392951aa3ae1b2b4ae8
Saurabh Giri <81623624+saurabh9136@users.noreply.github.com>
2024-03-23 11:32:03 +0530

    solve question related to tuples

---
+++ Tuple_data_structure/practiceQuestions.py
@@ -0,0 +1,10 @@
+# WAP to count the number of students with the "A" grade in the following tuple.
+# ["C", "D", "A", "A", "B", "B", "A"]
+
+studentGrades = ("C", "D", "A", "A", "B", "B", "A")
+print(studentGrades.count("A"))
+
+# Store the above values in a list & sort them from “A” to “D”.
+list = ["C", "D", "A", "A", "B", "B", "A"]
+list.sort()
+print(list)
