b316f0a6338c2e9541aa700a7e535b82dd7b3e5b
Saurabh Giri <81623624+saurabh9136@users.noreply.github.com>
2024-03-23 11:31:09 +0530

    learned tuple

---
+++ Tuple_data_structure/intro.py
@@ -0,0 +1,18 @@
+"""
+Tuples in Python
+A built-in data type that lets us create immutable sequences of values.
+
+tup = (87, 64, 33, 95,76) #tup[0], tup[1]..
+tup[0] = 43 #NOT allowed in python
+"""
+
+# To declare a Tuple use ()
+
+tuple = (3, 2, 5, 1)
+
+#correct way to declare single value tuple
+singleTuple = (4,) # help compiler understand better, compalsory
+
+nullTup = ()
+
+print(type(tuple))
