e13d1f94009c4e53b16e0eb05a310a17d3f6f6ab
Saurabh Giri <81623624+saurabh9136@users.noreply.github.com>
2024-03-26 10:05:02 +0530

    learn about set

---
+++ set_data_structure/intro.py
@@ -0,0 +1,17 @@
+"""
+set is the collection of unorderd/no index items
+Each element in the set must be unique & immutable
+"""
+
+#*********** SET IS MUTABLE BUT SET ELEMENTS ARE IMMUTABLE *********************
+
+# set is denoted by {} difference is here we need to pass only values compaing to dict
+collection = {1, 2, 4, 5, "hello", "I", "am", "Saurabh", 22}
+
+# to init the empty set need to use below syntax
+collection_null = set()
+
+
+
+print(collection)
+print(type(collection_null))
