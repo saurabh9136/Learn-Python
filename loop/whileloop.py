fc42f61aa8fd207843547a75272c28a69758d8ca
Saurabh Giri <81623624+saurabh9136@users.noreply.github.com>
2024-03-28 11:02:21 +0530

    started with loops

---
+++ loop/whileloop.py
@@ -0,0 +1,10 @@
+# syntax
+# Loops are used to repeat instructions. 
+# while condition : 
+
+# print the value 10 times
+user_input = int(input("Enter a number"))
+
+while user_input > 0 :
+    print("hellow world")
+    user_input = user_input - 1
