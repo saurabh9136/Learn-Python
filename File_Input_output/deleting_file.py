d94193fad2738ae2f102d33c0c35de92c7e4a294
Saurabh Giri <81623624+saurabh9136@users.noreply.github.com>
2024-03-29 10:22:04 +0530

    Deleting file

---
+++ File_Input_output/deleting_file.py
@@ -0,0 +1,9 @@
+import os
+"""
+Deleting a File
+using the os module
+
+Module (like a code library) is a file written by another programmer that generally has
+a functions we can use.
+"""
+os.remove("D:\\Personal\\learn Python\\File_Input_output\\sample.txt")
