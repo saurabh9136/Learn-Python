f686ddc106c3d6e1591474db243f7bc3156ece93
Saurabh Giri <81623624+saurabh9136@users.noreply.github.com>
2024-03-29 10:19:29 +0530

    Introduction to file handling

---
+++ File_Input_output/intro.py
@@ -0,0 +1,27 @@
+"""
+File I/O in Python
+
+Python can be used to perform operations on a file. (read & write data)
+
+Types of all files
+1.Text Files : .txt, .docx, .log etc.
+2.Binary Files: mp4, .mov, .png, .jpeg etc
+"""
+
+"""
+Open, read & close File
+
+We have to open a file before reading or writing.
+
+f = open( “file_name”,“mode”)
+
+r : read mode
+w : write mode
+
+sample.txt
+demo.docx
+
+data = f.read( )
+
+f.close( )
+"""
