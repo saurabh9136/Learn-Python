f18a6edbf2417407da2e6ded608666d267968466
Saurabh Giri <81623624+saurabh9136@users.noreply.github.com>
2024-03-29 10:21:38 +0530

    Manipulating data within file

---
+++ File_Input_output/file_write.py
@@ -0,0 +1,17 @@
+"""
+
+#overwrites the entire file
+f = open( “demo.txt”,“w”) write w+ read + write - truncate
+f.write( “this is a new line“ )
+
+
+Writing to a file
+f = open( “demo.txt”,“a”) append a+ read + append - no truncate
+f.write( “this is a new line“ ) #adds to the file
+"""
+
+
+f = open("D:\\Personal\\learn Python\\File_Input_output\\demo.txt", "a")
+f.write("\n and always try to stay within nature")
+f.close()
+
